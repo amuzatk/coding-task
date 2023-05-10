068abe63eeaf7a14c541c9798573ca1be66f2042
Kazmatics <amuzatk@gmail.com>
2023-05-10 05:51:12 +0100

    Adjusted navbar

--- src/app/components/men/men.component.ts
+++ src/app/components/men/men.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CartServiceService } from 'src/app/cart-service.service';
 
 @Component({
   selector: 'app-men',
@@ -10,6 +11,8 @@ import { Router } from '@angular/router';
   title = 'shopping-cart-app';
   capacity: any;
   states: any;
+  cartData: any[] = [];
+  selectedItem: any;
   // groupedCities: SelectItemGroup[];
   selectedCurrency = '$USD';
   selectedCity: any;
@@ -35,125 +38,50 @@ import { Router } from '@angular/router';
     this.counter--;
   }
 
-  constructor(private router:Router) {
+  constructor(private router:Router , private cartServiceService: CartServiceService) {
   }
 
   ngOnInit() {
-    this.loadCapacity, this.loadStates; this.test;
-
-    this.cart = [
-      {
-        image: 'Image1.png',
-          text: "Client",
-          // text: 'Apollo Running Short',
-          amount: '$50.000'
-      },
-      {
-        image: 'Image2.png',
-        text: "Client",
-        amount: '$50.000'
-      },
-      {
-        image: 'Image3.png',
-        text: "Client",
-        amount: '$50.000'
-      },
-      {
-        image: 'Image4.png',
-        text: "Client",
-        amount: '$50.000'
-      },
-      {
-        image: 'Image1.png',
-        text: "Client",
-        amount: '$50.000'
-      },
-      {
-        image: 'Image3.png',
-        text: "Client",
-        amount: '$50.000'
-      },
-  ];
-  }
-  loadCapacity() {
-    this.capacity = [
-      { name: '1-5', code: '1-5' },
-      { name: '6-10', code: '6-10' },
-      { name: '11-18', code: '11-18' },
-      { name: '19-32', code: '19-32' },
-      { name: '33 and above', code: '33 and above' },
-    ];
-  }
-  loadStates() {
-    this.states = [
-      { name: 'Abia', code: 'AB' },
-      { name: 'Adamawa', code: 'AD' },
-      { name: 'Akwa Ibom', code: 'AK' },
-      { name: 'Anambra', code: 'AN' },
-      { name: 'Bauchi', code: 'BA' },
-      { name: 'Bayelsa', code: 'BS' },
-      { name: 'Benue', code: 'BN' },
-      { name: 'Borno', code: 'BR' },
-      { name: 'Cross River', code: 'CR' },
-      { name: 'Delta', code: 'DT' },
-      { name: 'Ebonyi', code: 'EB' },
-      { name: 'Edo', code: 'ED' },
-      { name: 'Ekiti', code: 'EK' },
-      { name: 'Enugu', code: 'EN' },
-      { name: 'FCT - Abuja', code: 'FCT' },
-      { name: 'Gombe', code: 'GB' },
-      { name: 'Imo', code: 'IM' },
-      { name: 'Jigawa', code: 'JG' },
-      { name: 'Kaduna', code: 'KD' },
-      { name: 'Kano', code: 'KN' },
-      { name: 'Katsina', code: 'KT' },
-      { name: 'Kebbi', code: 'KB' },
-      { name: 'Kogi', code: 'KG' },
-      { name: 'Kwara', code: 'KW' },
-      { name: 'Lagos', code: 'LA' },
-      { name: 'Nasarawa', code: 'NA' },
-      { name: 'Niger', code: 'NG' },
-      { name: 'Ogun', code: 'OG' },
-      { name: 'Ondo', code: 'OD' },
-      { name: 'Osun', code: 'OS' },
-      { name: 'Oyo', code: 'OY' },
-      { name: 'Plateau', code: 'PL' },
-      { name: 'Rivers', code: 'RV' },
-      { name: 'Sokoto', code: 'SK' },
-      { name: 'Taraba', code: 'TR' },
-      { name: 'Yobe', code: 'YB' },
-      { name: 'Zamfara', code: 'ZM' },
-    ];
+    this.cartServiceService.getCartData().then((data: any) => {
+      this.cart = data;   
+    });
+  //   this.cart = [
+  //     {
+  //       image: 'Image1.png',
+  //         text: "Client",
+  //         // text: 'Apollo Running Short',
+  //         amount: '$50.000'
+  //     },
+  //     {
+  //       image: 'Image2.png',
+  //       text: "Client",
+  //       amount: '$50.000'
+  //     },
+  //     {
+  //       image: 'Image3.png',
+  //       text: "Client",
+  //       amount: '$50.000'
+  //     },
+  //     {
+  //       image: 'Image4.png',
+  //       text: "Client",
+  //       amount: '$50.000'
+  //     },
+  //     {
+  //       image: 'Image1.png',
+  //       text: "Client",
+  //       amount: '$50.000'
+  //     },
+  //     {
+  //       image: 'Image3.png',
+  //       text: "Client",
+  //       amount: '$50.000'
+  //     },
+  // ];
   }
 
-  test(){
-  this.info = [
-    {
-        name: "Client",
-        image: 'user.png',
-        show: false,
-        infomation: [
-       
-            {
-              id: 1,
-              name: "Add/Manage Client",
-              routeLink: "/admin/arc"
-            }
-        ],
-    },
-    {
-        name: "Payment Management",
-        image: 'user.png',
-        show: false,
-        infomation: [
-            {
-                id: 1,
-                name: "Payment Transaction",
-                routeLink: "/admin/payment/:id",
-            },
-        ],
-    },
-];
-}
+  goToProductDetail(id: string) {
+    this.router.navigate(['/detailed', id]);
+  }
 
 }
